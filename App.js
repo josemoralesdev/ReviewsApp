8879dedc767f91776c2f5bf6e7f367ebc5e48018
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-22 19:59:45 -0700

    Added custom fonts, initial files, structure & global styles.

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+import AppLoading from 'expo-app-loading'
+import { useFonts, PlayfairDisplay_400Regular } from '@expo-google-fonts/playfair-display'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PlayfairDisplay_400Regular
+  })
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/reviewDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function ReviewDetails() {
+    return (
+        <View style={styles.container}>
+            <Text>Review Details Screen</Text>
+        </View>
+    )
+}
