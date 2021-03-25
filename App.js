689c9bfea71ead8074af747c1c34c1105335ae96
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-25 12:36:58 -0700

    Wrapped App in SafeAreaProvider.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading'
 import { useFonts, PlayfairDisplay_400Regular } from '@expo-google-fonts/playfair-display'
 import Navigator from './routes/drawer'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,7 +13,9 @@ export default function App() {
     return <AppLoading />
   }
   return (
-    <Navigator />
+    <SafeAreaProvider>
+      <Navigator />
+    </SafeAreaProvider>
   );
 }
 
