265c36606acecc28d568a8487e5f88d250a9e20d
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-24 21:10:56 -0700

    Wrapped listing to card component, added styles & image display rating.

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { globalStyles } from '../styles/global'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import Card from '../shared/card';
+import { globalStyles, ratingImages } from '../styles/global'
 
 export default function ReviewDetails({ navigation, route }) {
     const { title, body, rating } = route.params;
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>{title}</Text>
-            <Text style={globalStyles.paragraph}>{body}</Text>
-            <Text >{rating}</Text>
+            <Card>
+                <Text style={globalStyles.titleText}>{title}</Text>
+                <Text style={globalStyles.paragraph}>{body}</Text>
+                <View style={styles.rating}>
+                    <Text>Reviews rating:</Text>
+                    <Image source={ratingImages[rating]} />
+                </View>
+            </Card>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+        marginTop: 16,
+        paddingTop: 16,
+        justifyContent: 'center'
+    }
+})
