18c275e54a8b8b1386e97377791cbe95589e1e4e
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-24 21:07:14 -0700

    Imported Card component, wrapped list text to card component, modified list ratings.

--- screens/home.js
+++ screens/home.js
@@ -2,15 +2,16 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import Card from '../shared/card';
 import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
     const [reviews, setReviews] = useState([
-        { title: 'Hulk', rating: 4.3, body: 'lorem ipsum', key: '1' },
-        { title: 'Ant Man', rating: 4.2, body: 'lorem ipsum', key: '2' },
-        { title: 'Spider-Man', rating: 4.4, body: 'lorem ipsum', key: '3' },
-        { title: 'Thor Ragnarok', rating: 4.8, body: 'lorem ipsum', key: '4' },
-        { title: 'The Avenger\'s', rating: 4.7, body: 'lorem ipsum', key: '5' },
+        { title: 'Hulk', rating: 2, body: 'lorem ipsum', key: '1' },
+        { title: 'Ant Man', rating: 3, body: 'lorem ipsum', key: '2' },
+        { title: 'Spider-Man', rating: 3, body: 'lorem ipsum', key: '3' },
+        { title: 'Thor Ragnarok', rating: 4, body: 'lorem ipsum', key: '4' },
+        { title: 'The Avenger\'s', rating: 4, body: 'lorem ipsum', key: '5' },
     ]);
     // const pressHandler = () => {
     //     navigation.navigate('ReviewDetails')
@@ -23,7 +24,9 @@ export default function Home({ navigation }) {
                 data={reviews}
                 renderItem={({ item }) => (
                     <TouchableOpacity onPress={() => (navigation.navigate('ReviewDetails', item))}>
-                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                        <Card>
+                            <Text style={globalStyles.titleText}>{item.title}</Text>
+                        </Card>
                     </TouchableOpacity>
                 )}
             />
